49c103f63190445be36b71fae95971caaf1b7755
Orri Ólafsson <orri14@i-7f3b5dd9.csd07cloud.internal>
2015-11-03 15:24:45 +0000

    Added Valid input function and tests

--- src/main/java/is/ru/MILLA/BusinessLogic.java
+++ src/main/java/is/ru/MILLA/BusinessLogic.java
@@ -3,21 +3,36 @@ package is.ru.MILLA;
 
 public class BusinessLogic
 {
-    private Player player1;
-    private Player player2;
-    private Grid grid;
+	private Player player1;
+	private Player player2;
+	private Grid grid;
 
 
-    public BusinessLogic(Player newPlayer1, Player newPlayer2)
-    {
-            player1 = newPlayer1;
-            player2 = newPlayer2;
-            grid = new Grid();
-    }
+   
+	public BusinessLogic(Player newPlayer1, Player newPlayer2)
+        {
+                player1 = newPlayer1;
+                player2 = newPlayer2;
+		grid = new Grid();
+ 	}	
 
-    public String getGrid()
-    {
-            return grid.toString();
-    }
+   	public String getGrid()
+   	{
+		return grid.toString();
+   	}
+
+	public boolean isValidInput(int input)
+        {
+                Node[] nodes = grid.getNodes();
+
+		if(input < 1 || input > 9){
+			return false;
+                }else if(nodes[input-1].isEmpty()){
+                        return true;
+                }else{
+                        return false;
+                }
+        }	
+	
 
 }
--- src/test/java/is/ru/MILLA/BusinessLogicTest.java
+++ src/test/java/is/ru/MILLA/BusinessLogicTest.java
@@ -21,8 +21,69 @@ public class BusinessLogicTest {
 
                 assertEquals(compareString, bl.getGrid());
         }
+	
+	@Test
+        public void testValidInputOne()
+        {
+		Player player1 = new Player(1, "Morgan");
+                Player player2 = new Player(2, "Nicolas");
+
+                BusinessLogic bl = new BusinessLogic(player1, player2);
+
+        	assertEquals(true, bl.isValidInput(1));
+
+        }
 
 
+
+        @Test
+        public void testValidInputNine()
+        {
+		Player player1 = new Player(1, "Morgan");
+                Player player2 = new Player(2, "Nicolas");
+
+                BusinessLogic bl = new BusinessLogic(player1, player2);
+
+        	assertEquals(true, bl.isValidInput(9));
+
+        }
+
+
+        @Test
+        public void testWrongInputZero()
+        {
+		Player player1 = new Player(1, "Morgan");
+                Player player2 = new Player(2, "Nicolas");
+
+                BusinessLogic bl = new BusinessLogic(player1, player2);
+
+        	assertEquals(false, bl.isValidInput(0));
+        }
+
+
+        @Test
+        public void testWrongInputTen()
+        {
+		Player player1 = new Player(1, "Morgan");
+                Player player2 = new Player(2, "Nicolas");
+
+                BusinessLogic bl = new BusinessLogic(player1, player2);
+
+        	assertEquals(false, bl.isValidInput(10));
+        }
+
+        @Test
+        public void testWrongInputNegative()
+        {
+		Player player1 = new Player(1, "Morgan");
+                Player player2 = new Player(2, "Nicolas");
+
+                BusinessLogic bl = new BusinessLogic(player1, player2);
+
+        	assertEquals(false, bl.isValidInput(-1));
+        }
+	
+	
 }
 
 
